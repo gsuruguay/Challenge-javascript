92726d0e83a84c88046c8c01a52106a026bab8ce
gsuruguay <guillermo.suruguay@gmail.com>
2021-09-15 15:17:38 -0300

    Testing database

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require('mysql');
 const myConnection = require("express-myconnection");
 
-const app = express ();
+const app = express();
 
 app.use(myConnection(mysql, { // A ESTO DEBO HACER VARIABLE DE ENTORNOS
     host: 'localhost',
@@ -19,29 +19,43 @@ app.set("port", process.env.PORT || 3333)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.json({
         msg: "Este es el home"
     })
 })
 
-app.get("/operations", (req, res)=>{
-    res.json({
-        msg: "AQUI VA EL FORM DE ABM OPERATIONS"
+app.get("/operations", (req, res) => {
+    req.getConnection((error, conn) => {
+        if (error) {
+            res.status(500).send('Connection error')
+        }
+        conn.query('SELECT concept, amount, date, type FROM operations', (err, operation) => {
+            if (err) {
+                res.status(400).json({
+                    msg: 'There was an error consulting the operations',
+                    err
+                })
+            }
+            console.log(operation)
+            res.json({ 
+                operation
+            })
+        })
     })
 })
 
-app.post("/createOperation", (req, res)=>{
+app.post("/createOperation", (req, res) => {
     const newOperation = req.body;
 })
 
-app.put("/setOperation/:id", (req, res)=>{
+app.put("/setOperation/:id", (req, res) => {
     const { id } = req.params
     const setOperation = req.body
 })
 
 
 //Starting the server
-app.listen(app.get("port"), ()=>{
+app.listen(app.get("port"), () => {
     console.log(`server on ${app.get("port")}`);
 })
